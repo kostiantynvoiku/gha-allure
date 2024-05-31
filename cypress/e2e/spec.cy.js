bee2cfdd27829d5b92867b2106f8dd56d5580319
Konstantin V <kostiantyn.voiku@gmail.com>
2024-05-31 21:41:14 +0300

    1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
+    console.log('USER: ', Cypress.env('USER'))
+    console.log('PASSWORD: ', Cypress.env('PASSWORD'))
+    console.log('STRIPE_KEY: ', Cypress.env('stripeKey'))
     cy.visit('https://example.cypress.io')
     cy.get('#utilities').should('exist')
   })
