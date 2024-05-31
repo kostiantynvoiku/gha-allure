b15c4a35d638ff59ed187550e4e3585e5a038549
Konstantin V <kostiantyn.voiku@gmail.com>
2024-05-31 21:49:14 +0300

    1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    expect(Cypress.env('user')).to.eq('kostiantyn.voiku@qodeup.com')
+    expect(Cypress.env('USER')).to.eq('kostiantyn.voiku@qodeup.com')
     expect(Cypress.env('password')).to.eq('123qwe123QWE')
     expect(Cypress.env('stripeKey')).to.eq('pk_test_DJYoIp8py8PUFfgCySGnS3Nu00phlssDq3')
     cy.visit('https://example.cypress.io')
