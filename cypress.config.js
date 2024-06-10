875fe48c085bd62e08c5de6c7ac669cfc957c11a
Konstantin V <kostiantyn.voiku@gmail.com>
2024-06-10 23:00:45 +0300

    1

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,26 @@
 const { defineConfig } = require("cypress");
 const { allureCypress } = require("allure-cypress/reporter");
 const cypressSplit = require('cypress-split')
-const { beforeRunHook } = require('cypress-mochawesome-reporter/lib');
+//const { beforeRunHook } = require('cypress-mochawesome-reporter/lib');
 
 
 module.exports = defineConfig({
   // experimentalInteractiveRunEvents: true,
-  video: true,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   e2e: {
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // require('cypress-mochawesome-reporter/plugin')(on);
-      on('before:run', async (details) => {
-        await beforeRunHook(details);
-      });
+      // on('before:run', async (details) => {
+      //   await beforeRunHook(details);
+      // });
       allureCypress(on);
       cypressSplit(on, config)
       return config;
